0f77501cc0f2a0004e8e470e077411f368fdc335
Ojekunle Adeola <adeolaoluwa24@gmail.com>
2024-03-10 00:21:55 +0100

    Added user edit profile, skill and projects functionality

--- users/views.py
+++ users/views.py
@@ -4,11 +4,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 
 
 # Create your views here.
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect("profiles")
@@ -37,10 +38,34 @@ def logoutUser(request):
     messages.error(request,'User was successfully logged out ')
     return redirect('login')
 
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user= form.save(commit=False)
+            user.username=user.username.lower()
+            user.save()
+
+            messages.success(request, "User account has been created")
+            login(request, user)
+            return redirect('edit-account')
+        else:
+            messages.error(request, "An error has occured during registration")
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
+
 def profiles(requests):
     profiles=Profile.objects.all()
     context= {'profiles':profiles}
     return render(requests, 'users/profiles.html', context)
+
+
+
 def userProfile(request, pk):
     profile=Profile.objects.get(id=pk)
     topSkills =profile.skill_set.exclude(description__exact="")
@@ -48,3 +73,72 @@ def userProfile(request, pk):
     
     context={'profile':profile, 'topSkills':topSkills,'otherSkills':otherSkills,}
     return render(request,'users/user-profile.html', context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile=request.user.profile
+    skills =profile.skill_set.all()
+    projects =profile.project_set.all()
+
+    context ={'profile':profile, 'skills':skills,'projects':projects }
+    return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def  editAccount(request):
+    profile = request.user.profile
+    form= ProfileForm(instance=profile)
+
+    if request.method== "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+    context= {'form': form, }
+    return render(request, 'users/profile_form.html', context)
+
+@login_required(login_url='login')
+def createSkill(request):
+    profile = request.user.profile
+    form = SkillForm()
+
+    if request.method == "POST":
+        form = SkillForm(request.POST)
+        if form.is_valid():
+            skill=form.save(commit=False)
+            skill.owner = profile
+            skill.save()
+            messages.success(request, "Skill was added successfully")
+            return redirect('account')
+
+    context={'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+@login_required(login_url='login')
+def updateSkill(request, pk):
+    profile = request.user.profile
+    skill=profile.skill_set.get(id=pk)
+    form = SkillForm(instance=skill)
+
+    if request.method == "POST":
+        form = SkillForm(request.POST, instance=skill)
+        if form.is_valid():
+            
+            form.save()
+            messages.success(request, "Skill was updated successfully")
+            return redirect('account')
+
+    context={'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+def deleteSkill(request, pk):
+    profile=request.user.profile
+    skill = profile.skill_set.get(id=pk)
+    if request.method == "POST":
+        skill.delete()
+        messages.success(request, "Skill was deleted successfully")
+        return redirect("account")
+    context ={'object':skill}
+    return render(request, 'delete_template.html', context)
